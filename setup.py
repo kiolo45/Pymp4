0d9b6c79878fe665d9a001ea9b8b26e3c5eaf9be
JuanPerdomo00 <perdomojuan187@gmail.com>
2022-04-17 14:01:00 -0500

    se agrega setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import platform
+import os
+import time
+
+
+print("*"*49)
+print("[*] Instalador de dependencias by: jakepy [*]")
+print("*"*49)
+
+try:
+    import colorama
+    import pyfiglet
+    import pafy
+    print("[*] Ya tiene todo instalado")
+    exit()
+
+except ImportError:
+    print("[!] No tiene modulos instalados")
+    time.sleep(1.5)
+    print("[*] Descargando modulos faltantes")
+    time.sleep(1.2)
+    os.system("pip3 install -r requirements.txt")
+    time.sleep(1.5)
+    print("[*] Instalado...")
